dfc9c8a45a433dea29642bb0f11c3bfaffd90e0b
vanjobanjo <vau.ce1.vb@gmail.com>
2021-10-31 23:01:13 +0100

    project structure

---
+++ src/main/java/de.fhwedel.klausps_examples.controller/Main.java
@@ -0,0 +1,12 @@
+package de.fhwedel.klausps_examples.controller;
+
+/**
+ * project structure
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("this is the project!");
+    }
+
+}
