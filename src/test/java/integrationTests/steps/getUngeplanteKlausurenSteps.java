0713657b3e80576f33af5cb2fff7cb8124361449
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2021-11-01 20:14:19 +0100

    resolves #4 shifted repository with dependencies from pom.xml to maven.yml for use in CI pipeline

---
+++ src/test/java/integrationTests/steps/getUngeplanteKlausurenSteps.java
@@ -0,0 +1,41 @@
+package integrationTests.steps;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.de.Angenommen;
+import io.cucumber.java.de.Dann;
+import io.cucumber.java.de.Und;
+import io.cucumber.java.de.Wenn;
+import org.junit.AssumptionViolatedException;
+
+public class getUngeplanteKlausurenSteps extends BaseSteps {
+
+    @Angenommen("es existieren die folgenden Klausuren:")
+    public void esExistierenDieFolgendenKlausuren(DataTable dataTable) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Wenn("ich alle ungeplanten Klausuren anfrage")
+    public void ichAlleUngeplantenKlausurenAnfrage() {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Dann("bekomme ich die Klausuren {string}")
+    public void bekommeIchDieKlausuren(String klausuren) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Dann("bekomme ich keine Klausuren")
+    public void bekommeIchKeineKlausuren() {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Angenommen("es existieren keine Klausuren")
+    public void esExistierenKeineKlausuren() {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Dann("bekomme ich den Block als Teil der ungeplanten Klausuren")
+    public void bekommeIchDenBlockAlsTeilDerUngeplantenKlausuren(DataTable dataTable) {
+
+    }
+}
