5920cc4ecc13aca4160c17377d7f73d45348b892
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2021-11-15 17:00:01 +0100

    Feature getGeplanteKlausuren (#25)
    resolves #8 
    added integration test for verification of the feature to get all planned "Klausuren"

---
+++ src/test/java/integrationTests/steps/getGeplanteKlausurenSteps.java
@@ -0,0 +1,11 @@
+package integrationTests.steps;
+
+import io.cucumber.java.de.Wenn;
+
+public class getGeplanteKlausurenSteps extends BaseSteps {
+
+    @Wenn("ich alle geplanten Klausuren anfrage")
+    public void ichAlleGeplantenKlausurenAnfrage() {
+    }
+
+}
