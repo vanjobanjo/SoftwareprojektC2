09258c33f9571332e41e5cca2c24ef3f0161f271
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2021-11-20 21:48:16 +0100

    cleaned up phrasing and added list functionality

--- src/test/java/integrationTests/steps/removePruefungVonBlockSteps.java
+++ src/test/java/integrationTests/steps/removePruefungVonBlockSteps.java
@@ -1,60 +1,111 @@
 package integrationTests.steps;
 
+import io.cucumber.java.ParameterType;
 import io.cucumber.java.de.Angenommen;
 import io.cucumber.java.de.Dann;
+import io.cucumber.java.de.Und;
 import io.cucumber.java.de.Wenn;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.AssumptionViolatedException;
 
 
 public class removePruefungVonBlockSteps {
 
-
-    //TODO eigentlich moechte ich gerne, dass hier ein List<String> mit pruefungen uebergeben werden kann
-    @Angenommen("der geplante Block {string} hat die Pruefungen {}")
-    public void derBlockGeplanteHatDiePruefungenListeVonPruefungenInnerhalbEinesBlockes(String block, String pruefungen) {
-
+    @ParameterType("(.*(, .*)*)")
+    public List<String> stringList(String strings) {
+        return Arrays.stream(strings.split(", ")).collect(Collectors.toList());
     }
 
-    @Wenn("die {int} Pruefung aus dem Block entfernt wird")
-    public void diePruefungAusDemBlockEntferntWird(int index) {
-
+    //TODO das muss evtl geaendert werden
+    @Und("das Entfernen hat Auswirkungen auf das Scoring")
+    public void hatDasEntfernenAuswirkungenAufDasScoring() {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Dann("ist {int} Pruefung ungeplant")
-    public void istPruefungUngeplant(int index) {
-
+    @Angenommen("die {stringList} sind Teil des geplanten Block {string}")
+    public void dieKlausurenSindTeilDesGeplantenBlock(List<String> klausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Angenommen("der ungeplante Block {string} hat die Pruefungen {}")
-    public void derUngeplanteBlockHatDiePruefungenListeVonPruefungenInnerhalbEinesBlockes(String block, String pruefungen) {
+    @Wenn("die erste der {stringList} aus dem Block entfernt wird")
+    public void dieErsteDerKlausurenAusDemBlockEntferntWird(List<String> klausuren) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Dann("ist {int} Prufung nicht mehr im Block {string}")
-    public void istPrufungNichtMehrImBlock(int index, String block) {
+    @Dann("ist die erste der {stringList} ungeplant")
+    public void istDieErsteDerKlausurenUngeplant(List<String> klausuren) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Dann("ist Block {string} immernoch ungeplant")
-    public void istBlockImmernochUngeplant(String block) {
+    @Und("die erste der {stringList} ist nicht mehr im Block {string}")
+    public void dieErsteDerKlausurenIstNichtMehrImBlock(List<String> klausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Dann("ist Block {string} immernoch geplant")
-    public void istBlockImmernochGeplant(String block) {
+    @Und("der Block {string} ist immernoch geplant")
+    public void derBlockIstImmernochGeplant(String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    @Dann("hat der Block {string} noch {int} Pruefungen")
-    public void hatDerBlockNochPruefungen(String block, int anzahlPruefungen) {
+    @Und("es sind {int} Klausuren im Block {string}")
+    public void esSindKlausurenImBlock(int amountKlausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Angenommen("der geplante Block {string} hat die Pruefung {string}")
+    public void derGeplanteBlockHatDiePruefung(String blockName, String klausurName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Wenn("{string} aus dem Block entfernt wird")
+    public void ausDemBlockEntferntWird(String klausurName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Dann("ist {string} ungeplant")
+    public void istUngeplant(String klausurName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Und("der Block {string} ist noch geplant")
+    public void derBlockIstNochGeplant(String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Und("der Block {string} hat keine Pruefungen mehr")
+    public void derBlockHatKeinePruefungenMehr(String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Angenommen("die {stringList} sind Teil des ungeplanten Block {string}")
+    public void dieKlausurenSindTeilDesUngeplantenBlock(List<String> klausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Und("der Block {string} ist immer noch ungeplant")
+    public void derBlockIstImmerNochUngeplant(String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
+    }
+
+    @Angenommen("{stringList} sind Teil des geplanten Block {string}")
+    public void sindTeilDesGeplantenBlock(List<String> klausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
     @Wenn("die Pruefung {string} aus dem Block {string} entfernt wird")
-    public void diePruefungAusDemBlockEntferntWird(String pruefung, String block) {
+    public void diePruefungAusDemBlockEntferntWird(String klausurName, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
     @Dann("werfe IllegalArgumentException")
     public void werfeIllegalArgumentException() {
-
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 
-    //TODO das muss evtl geaendert werden
-    @Dann("hat das Entfernen Auswirkungen auf das Scoring")
-    public void hatDasEntfernenAuswirkungenAufDasScoring() {
+    @Angenommen("{stringList} sind Teil des ungeplanten Block {string}")
+    public void sindTeilDesUngeplantenBlock(List<String> klausuren, String blockName) {
+        throw new AssumptionViolatedException("Not implemented yet!");
     }
 }
