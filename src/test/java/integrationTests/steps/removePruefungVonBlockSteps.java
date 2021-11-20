d25b51f7fffa5f37572b53f4f5c8df700f451d91
Miwo <inf104162@fh-wedel.de>
2021-11-20 15:26:19 +0100

    feat: remove Pruqefung von Block

--- src/test/java/integrationTests/steps/removePruefungVonBlockSteps.java
+++ src/test/java/integrationTests/steps/removePruefungVonBlockSteps.java
@@ -3,14 +3,13 @@ package integrationTests.steps;
 import io.cucumber.java.de.Angenommen;
 import io.cucumber.java.de.Dann;
 import io.cucumber.java.de.Wenn;
-import java.util.List;
 
 
 public class removePruefungVonBlockSteps {
 
 
     //TODO eigentlich moechte ich gerne, dass hier ein List<String> mit pruefungen uebergeben werden kann
-    @Angenommen("der Block geplante {string} hat die Pruefungen {}")
+    @Angenommen("der geplante Block {string} hat die Pruefungen {}")
     public void derBlockGeplanteHatDiePruefungenListeVonPruefungenInnerhalbEinesBlockes(String block, String pruefungen) {
 
     }
@@ -25,4 +24,37 @@ public class removePruefungVonBlockSteps {
 
     }
 
+    @Angenommen("der ungeplante Block {string} hat die Pruefungen {}")
+    public void derUngeplanteBlockHatDiePruefungenListeVonPruefungenInnerhalbEinesBlockes(String block, String pruefungen) {
+    }
+
+    @Dann("ist {int} Prufung nicht mehr im Block {string}")
+    public void istPrufungNichtMehrImBlock(int index, String block) {
+    }
+
+    @Dann("ist Block {string} immernoch ungeplant")
+    public void istBlockImmernochUngeplant(String block) {
+    }
+
+    @Dann("ist Block {string} immernoch geplant")
+    public void istBlockImmernochGeplant(String block) {
+    }
+
+    @Dann("hat der Block {string} noch {int} Pruefungen")
+    public void hatDerBlockNochPruefungen(String block, int anzahlPruefungen) {
+    }
+
+    @Wenn("die Pruefung {string} aus dem Block {string} entfernt wird")
+    public void diePruefungAusDemBlockEntferntWird(String pruefung, String block) {
+    }
+
+    @Dann("werfe IllegalArgumentException")
+    public void werfeIllegalArgumentException() {
+
+    }
+
+    //TODO das muss evtl geaendert werden
+    @Dann("hat das Entfernen Auswirkungen auf das Scoring")
+    public void hatDasEntfernenAuswirkungenAufDasScoring() {
+    }
 }
