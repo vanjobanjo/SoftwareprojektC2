a062f1c98232118c70630d46968bd73fd716e7e5
Miwo <inf104162@fh-wedel.de>
2021-11-21 15:36:22 +0100

    feat: remove Pruqefung von Block

--- src/test/java/integrationTests/steps/teilnehmerKreisSchaetzungSteps.java
+++ src/test/java/integrationTests/steps/teilnehmerKreisSchaetzungSteps.java
@@ -2,6 +2,7 @@ package integrationTests.steps;
 
 import io.cucumber.java.de.Angenommen;
 import io.cucumber.java.de.Dann;
+import io.cucumber.java.de.Und;
 import io.cucumber.java.de.Wenn;
 
 public class teilnehmerKreisSchaetzungSteps {
@@ -11,16 +12,22 @@ public class teilnehmerKreisSchaetzungSteps {
 
     }
 
-    @Wenn("ich den Studiengang {string} Fachsemester {int} mit Ordnung {string} und {int} schaetze und hinzufuege")
-    public void ichDenStudiengangFachsemesterMitOrdnungUndSchaetzeUndHinzufuege(String studiengang, int sememster, String ordnung, int schaetzung) {
+    @Wenn("ich den Studiengang {string} Fachsemester {int} mit Ordnung {string} und {int} schaetze und in {string} hinzufuege")
+    public void teilnehmerKreisInKlausurHinzufuegen(
+            String studiengang, int sememster, String ordnung, int schaetzung, String klausur) {
     }
 
 
     @Dann("hat die Pruefung {string} die Teilnehmerkreischaetzungen: {string}")
-    public void hatDiePruefungDieTeilnehmerkreischaetzungen(String pruefung, String teilnehmerKreisSchaetzung) {
+    public void pruefungHatTeilnehmerkreis(String pruefung, String teilnehmerKreisSchaetzung) {
     }
 
     @Dann("werfe IllegalArgumentException")
     public void werfeIllegalArgumentException() {
+    }
+
+    @Und("es hat Auswirkungen auf das Scoring von {string}")
+    public void veraendertesScoring(String klausur) {
+
     }
 }
