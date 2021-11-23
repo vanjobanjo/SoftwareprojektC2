f09cdd4b5ff75841630efa4158782f767f52a711
vanjobanjo <44374624+vanjobanjo@users.noreply.github.com>
2021-11-23 17:18:42 +0100

    added assumption violated exceptions

--- src/test/java/de/fhwedel/klausps/controller/integrationTests/steps/getGeplanteBloecke.java
+++ src/test/java/de/fhwedel/klausps/controller/integrationTests/steps/getGeplanteBloecke.java
@@ -4,33 +4,41 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.de.Dann;
 import io.cucumber.java.de.Und;
 import io.cucumber.java.de.Wenn;
+import org.junit.AssumptionViolatedException;
 
 public class getGeplanteBloecke {
 
   @Und("es gibt die folgenden geplanten Bloecke:")
   public void esGibtDieBloecke(DataTable table) {
+    throw new AssumptionViolatedException("Not implemented yet!");
 
   }
 
   @Wenn("ich alle geplanten Bloecke anfrage")
   public void ichAlleGeplantenBloeckeAnfrage() {
+    throw new AssumptionViolatedException("Not implemented yet!");
   }
 
   @Dann("erhalte ich die Bloecke {string}")
   public void erhalteIchDieBloecke(String bloecke) {
+    throw new AssumptionViolatedException("Not implemented yet!");
 
   }
 
   @Dann("erhalte ich keine Bloecke")
   public void erhalteIchKeineBloecke() {
+    throw new AssumptionViolatedException("Not implemented yet!");
   }
 
   @Und("es gibt keine geplanten Bloecke")
   public void esGibtKeineGeplantenBloecke() {
+    throw new AssumptionViolatedException("Not implemented yet!");
 
   }
 
   @Und("es gibt die folgenden geplanten und ungeplanten Bloecke:")
   public void esGibtDieFolgendenGeplantenUndUngeplantenBloecke(DataTable table) {
+    throw new AssumptionViolatedException("Not implemented yet!");
   }
+
 }
