9ad4fc99cb8ee9e4c10b2bd955219de7d9c01940
miwox <77623967+miwox@users.noreply.github.com>
2021-11-29 19:31:54 +0100

    feat: getGeplanteBlöcke (#96)
    Co-authored-by: Miwo <inf104162@fh-wedel.de>

--- src/main/java/de/fhwedel/klausps/controller/api/builders/PruefungDTOBuilder.java
+++ src/main/java/de/fhwedel/klausps/controller/api/builders/PruefungDTOBuilder.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class PruefungDTOBuilder {
 
