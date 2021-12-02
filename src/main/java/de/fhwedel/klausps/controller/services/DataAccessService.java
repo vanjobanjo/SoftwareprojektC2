f716cd86853cc78ae9ed0cb546dc6f0569db70d9
miwox <77623967+miwox@users.noreply.github.com>
2021-12-02 17:35:29 +0100

    Unschedule pruefung (#98)
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    * feat: unschedulePruefung
    
    Co-authored-by: Miwo <inf104162@fh-wedel.de>

--- src/main/java/de/fhwedel/klausps/controller/services/DataAccessService.java
+++ src/main/java/de/fhwedel/klausps/controller/services/DataAccessService.java
@@ -8,9 +8,11 @@ import de.fhwedel.klausps.model.api.Planungseinheit;
 import de.fhwedel.klausps.model.api.Pruefung;
 import de.fhwedel.klausps.model.api.Pruefungsperiode;
 import de.fhwedel.klausps.model.api.Teilnehmerkreis;
-import de.fhwedel.klausps.model.impl.BlockImpl;
 import de.fhwedel.klausps.model.impl.PruefungImpl;
+
 import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,9 +20,12 @@ import java.util.stream.Collectors;
 public class DataAccessService {
 
   private Pruefungsperiode pruefungsperiode;
+  private final ScheduleService scheduleService;
 
   public DataAccessService(Pruefungsperiode pruefungsperiode) {
+
     this.pruefungsperiode = pruefungsperiode;
+    this.scheduleService = new ScheduleService(pruefungsperiode.geplantePruefungen());
   }
 
   public ReadOnlyPruefung createPruefung(
@@ -33,15 +38,16 @@ public class DataAccessService {
     Pruefung isAvailable = pruefungsperiode.pruefung(pruefungsNr);
     if (isAvailable == null) {
       // todo contains static values as it is unclear where to retreave the data from
-      Pruefung pruefungModel = new PruefungImpl(pruefungsNr, name, "", duration, null);
-      addTeilnehmerKreisSchaetzungToModelPruefung(pruefungModel, teilnehmerkreise);
-      pruefungsperiode.addPlanungseinheit(pruefungModel);
+      
+      pruefungsperiode.addPlanungseinheit(
+          new PruefungImpl(pruefungsNr, name, "", duration, null)); // TODO Valerio
       return new PruefungDTOBuilder()
           .withPruefungsName(name)
           .withPruefungsNummer(pruefungsNr)
           .withPruefer(pruefer)
           .withDauer(duration)
-          .withTeilnehmerKreisSchaetzung(teilnehmerkreise) //TODO an Valerio: davor stand da keySet()
+          .withTeilnehmerKreisSchaetzung(
+              teilnehmerkreise) // TODO an Valerio: davor stand da keySet()
           .build();
     }
     return null;
@@ -69,58 +75,112 @@ public class DataAccessService {
     return false;
   }
 
-  public ReadOnlyPruefung changeNameOfPruefung(ReadOnlyPruefung toChange, String name){
+  public ReadOnlyPruefung changeNameOfPruefung(ReadOnlyPruefung toChange, String name) {
     Pruefung pruefungModel = pruefungsperiode.pruefung(toChange.getPruefungsnummer());
+
+    if (pruefungModel == null) {
+      throw new IllegalArgumentException();
+    }
+
     pruefungModel.setName(name);
     int scoring = toChange.getScoring();
     return new PruefungDTOBuilder(pruefungModel).withScoring(scoring).build();
   }
 
-  public Set<ReadOnlyPruefung> getGeplantePruefungen(){
-    return pruefungsperiode
-            .geplantePruefungen()
-            .stream()
-            .map(pruefung -> new PruefungDTOBuilder(pruefung) //TODO pruefung.getScoring(); Scoring berechnen
+  public Set<ReadOnlyPruefung> getGeplantePruefungen() {
+    return pruefungsperiode.geplantePruefungen().stream()
+        .map(
+            pruefung ->
+                new PruefungDTOBuilder(pruefung)
+                    /*                    .withScoring(
+                    scheduleService.getScoring(
+                        pruefung,
+                        pruefungsperiode.block(pruefung) == null
+                            ? new LinkedList<>()
+                            : pruefungsperiode.block(pruefung).getPruefungen().stream()
+                                .toList())) */
                     .build())
-            .collect(Collectors.toSet());
+        .collect(Collectors.toSet());
   }
 
-  public Set<ReadOnlyPruefung> getUngeplanteKlausuren(){
-    return pruefungsperiode
-            .ungeplantePruefungen()
-            .stream()
-            .map(pruefung -> new PruefungDTOBuilder(pruefung) //TODO pruefung.getScoring();
+  public Set<ReadOnlyPruefung> getUngeplantePruefungen() {
+    return pruefungsperiode.ungeplantePruefungen().stream()
+        .map(
+            pruefung ->
+                new PruefungDTOBuilder(pruefung) // TODO pruefung.getScoring();
                     .build())
-            .collect(Collectors.toSet());
+        .collect(Collectors.toSet());
   }
 
-  public Set<ReadOnlyBlock> getGeplanteBloecke(){
-    return pruefungsperiode
-            .geplanteBloecke()
-            .stream().map(x ->
-                    new BlockDTO("TODO", //TODO
-                            x.getStartzeitpunkt(),
-                            x.getDauer(),
-                            x.isGeplant(),
-                            x.getPruefungen()
-                                    .stream()
-                                    .map(pruefung -> new PruefungDTOBuilder(pruefung).build())
-                                    .collect(Collectors.toSet())
-                    )).collect(Collectors.toSet());
+  public Set<ReadOnlyBlock> getGeplanteBloecke() {
+    return pruefungsperiode.geplanteBloecke().stream()
+        .map(
+            x ->
+                new BlockDTO(
+                    "TODO", // TODO
+                    x.getStartzeitpunkt(),
+                    x.getDauer(),
+                    x.isGeplant(),
+                    x.getPruefungen().stream()
+                        .map(
+                            pruefung ->
+                                new PruefungDTOBuilder(pruefung)
+                                    /*                                    .withScoring(
+                                    scheduleService.getScoring(
+                                        pruefung, x.getPruefungen().stream().toList()))*/
+                                    .build())
+                        .collect(Collectors.toSet())))
+        .collect(Collectors.toSet());
   }
 
-  public Set<ReadOnlyBlock> getUngeplanteBloecke(){
-    return pruefungsperiode
-            .ungeplanteBloecke()
-            .stream().map(x ->
-                    new BlockDTO("TODO", //TODO
-                            x.getStartzeitpunkt(),
-                            x.getDauer(),
-                            x.isGeplant(),
-                            x.getPruefungen()
-                                    .stream()
-                                    .map(pruefung -> new PruefungDTOBuilder(pruefung).build())
-                                    .collect(Collectors.toSet())
-                    )).collect(Collectors.toSet());
+  public Set<ReadOnlyBlock> getUngeplanteBloecke() {
+    return pruefungsperiode.ungeplanteBloecke().stream()
+        .map(
+            x ->
+                new BlockDTO(
+                    "TODO", // TODO
+                    x.getStartzeitpunkt(),
+                    x.getDauer(),
+                    x.isGeplant(),
+                    x.getPruefungen().stream()
+                        .map(pruefung -> new PruefungDTOBuilder(pruefung).build())
+                        .collect(Collectors.toSet())))
+        .collect(Collectors.toSet());
+  }
+
+  public List<ReadOnlyPruefung> unschedulePruefung(ReadOnlyPruefung pruefung) {
+    whenTrueThrowIllegalArgumentExpcetion(pruefung.ungeplant(), "Pruefung ist ungeplant!");
+
+    Pruefung pruefungModel = pruefungsperiode.pruefung(pruefung.getPruefungsnummer());
+
+    whenTrueThrowIllegalArgumentExpcetion(
+        pruefungModel == null, pruefung.getPruefungsnummer() + " Pruefung nicht in Periode");
+
+    whenTrueThrowIllegalArgumentExpcetion(
+        pruefungsperiode.block(pruefungModel) != null,
+        pruefung.getPruefungsnummer() + " Pruefung ist Teil eines Blockes");
+
+    return scheduleService.unschedulePruefung(pruefungModel).stream()
+        .map(
+            x ->
+                new PruefungDTOBuilder(x)
+                    .withScoring(
+                        scheduleService.getScoring(
+                            x,
+                            pruefungsperiode.block(x) == null
+                                ? new LinkedList<Pruefung>()
+                                : pruefungsperiode.block(x).getPruefungen().stream().toList()))
+                    .build())
+        .collect(Collectors.toList());
+  }
+
+  private void whenTrueThrowIllegalArgumentExpcetion(boolean condition, String text) {
+    if (condition) {
+      throw new IllegalArgumentException(text);
+    }
+  }
+
+  ScheduleService getScheduleService(){
+    return scheduleService;
   }
 }
