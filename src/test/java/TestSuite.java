33b2b17e50b1603bc020620be6d61d968004c242
vanjobanjo <44374624+vanjobanjo@users.noreply.github.com>
2021-12-04 20:30:59 +0100

    Pfadfinderregel

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,16 +1,10 @@
-package de.fhwedel.klausps.controller;
-
-
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 
 @RunWith(JUnitPlatform.class)
-@SelectPackages({"de.fhwedel.klausps.controller.api",
-    "de.fhwedel.klausps.controller.services"
-    // add packages here
-})
+@SelectPackages({"de.fhwedel.klausps.controller"})
 public class TestSuite {
   // don't write anything here
 }
