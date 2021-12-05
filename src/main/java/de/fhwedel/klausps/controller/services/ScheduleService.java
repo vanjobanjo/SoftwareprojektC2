f2a12381417515d971eeef553e1fa1ff4ca185d3
Miwo <inf104162@fh-wedel.de>
2021-12-05 13:27:37 +0100

    feat: Schedule Service Interface -> Tests angepasst -> Zum Planen und Umplanen: ScheduleService -> Pfadfinder

--- src/main/java/de/fhwedel/klausps/controller/services/ScheduleService.java
+++ src/main/java/de/fhwedel/klausps/controller/services/ScheduleService.java
@@ -1,6 +1,6 @@
 package de.fhwedel.klausps.controller.services;
 
-import de.fhwedel.klausps.controller.api.view_dto.ReadOnlyPruefung;
+
 import de.fhwedel.klausps.controller.exceptions.HartesKriteriumException;
 import de.fhwedel.klausps.model.api.Pruefung;
 
@@ -8,7 +8,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ScheduleService {
+public abstract class ScheduleService {
 
     /**
      * Plant eine übergebene Pruefung ein! Die uebergebene Pruefung muss Teil des Rueckgabewertes sein.
@@ -16,7 +16,7 @@ public interface ScheduleService {
      * @param termin - Starttermin
      * @return Liste von veränderten Ergebnissen
      */
-    List<Pruefung> schedulePruefung(Pruefung pruefung, LocalDateTime termin) throws HartesKriteriumException;
+    public abstract List<Pruefung> schedulePruefung(Pruefung pruefung, LocalDateTime termin) throws HartesKriteriumException;
 
     /**
      * Ändert die Dauer einer übergebenen Prüfung. Die übergebene Prüfung muss beim erfolgreichen
@@ -25,12 +25,12 @@ public interface ScheduleService {
      * @param minutes - die naue Dauer
      * @return Liste von Pruefung, jene die sich durch die Operation geändert haben.
      */
-    List<Pruefung> changeDuration(Pruefung pruefung, Duration minutes) throws HartesKriteriumException;
+    public abstract List<Pruefung> changeDuration(Pruefung pruefung, Duration minutes) throws HartesKriteriumException;
 
     /**
      * Gibt das Scoring zu einer übergebenen Pruefung zurück.
      * @param pruefung . Pruefung, dessen Scoring bestimmt werden soll
      * @return Scoring
      */
-    int scoringOfPruefung(Pruefung pruefung);
+    public abstract int scoringOfPruefung(Pruefung pruefung);
 }
--- src/test/java/de/fhwedel/klausps/controller/services/IOServiceTest.java
+++ src/test/java/de/fhwedel/klausps/controller/services/IOServiceTest.java
@@ -1,24 +1,19 @@
 package de.fhwedel.klausps.controller.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import de.fhwedel.klausps.model.api.Planungseinheit;
 import de.fhwedel.klausps.model.api.Pruefungsperiode;
 import de.fhwedel.klausps.model.api.Semester;
 import de.fhwedel.klausps.model.api.Semestertyp;
-import de.fhwedel.klausps.model.impl.PruefungsperiodeImpl;
+
 import de.fhwedel.klausps.model.impl.SemesterImpl;
 import java.time.LocalDate;
 import java.time.Year;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 
 class IOServiceTest {
 
