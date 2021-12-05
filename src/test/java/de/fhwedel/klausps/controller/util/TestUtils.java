cdf3fedab0793ea0590810b92b60a3e9954a9dd5
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2021-12-05 21:52:03 +0100

    introduction of testutils

---
+++ src/test/java/de/fhwedel/klausps/controller/util/TestUtils.java
@@ -0,0 +1,23 @@
+package de.fhwedel.klausps.controller.util;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class TestUtils {
+
+  public String getRandomString(Random random, int length) {
+    // code from https://www.baeldung.com/java-random-string modified
+    int leftLimit = 97; // letter 'a'
+    int rightLimit = 122; // letter 'z'
+    StringBuilder buffer = new StringBuilder(length);
+    for (int i = 0; i < length; i++) {
+      int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+      buffer.append((char) randomLimitedInt);
+    }
+    return buffer.toString();
+  }
+
+  public Duration getRandomDuration(Random random, int maxMinutes) {
+    return Duration.ofMinutes(random.nextInt(1, maxMinutes));
+  }
+}
