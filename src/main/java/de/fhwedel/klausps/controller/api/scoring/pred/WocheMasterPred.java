dcffecf146825ee39bbd27f8f5c07db5b455c489
Miwo <inf104162@fh-wedel.de>
2021-12-12 15:58:12 +0100

    feat: scheduleService

--- src/main/java/de/fhwedel/klausps/controller/api/scoring/pred/WocheMasterPred.java
+++ src/main/java/de/fhwedel/klausps/controller/api/scoring/pred/WocheMasterPred.java
@@ -1,6 +1,7 @@
 package de.fhwedel.klausps.controller.api.scoring.pred;
 
 import de.fhwedel.klausps.controller.kriterium.WeichesKriterium;
+import de.fhwedel.klausps.model.api.Ausbildungsgrad;
 import de.fhwedel.klausps.model.api.Pruefung;
 
 import java.time.LocalDateTime;
@@ -8,7 +9,6 @@ import java.util.Optional;
 
 public class WocheMasterPred extends WeichesKriteriumPred{
     private final LocalDateTime startMaster;
-
     public WocheMasterPred(LocalDateTime startOfMaster){
         super(WeichesKriterium.WOCHE_VIER_FUER_MASTER);
         startMaster = startOfMaster;
@@ -17,6 +17,6 @@ public class WocheMasterPred extends WeichesKriteriumPred{
     @Override
     public boolean test(Pruefung pruefung) {
         Optional<LocalDateTime> termin = Optional.ofNullable(pruefung.getStartzeitpunkt());
-        return termin.isEmpty() || termin.get().isAfter(startMaster);
+        return termin.isPresent() && termin.get().isAfter(startMaster) && pruefung.getAusbildungsgrade().stream().anyMatch(x -> x.equals(Ausbildungsgrad.MASTER));
     }
 }
