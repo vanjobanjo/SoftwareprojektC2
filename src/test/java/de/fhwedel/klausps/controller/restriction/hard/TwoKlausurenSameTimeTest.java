4a158997ca9357ae9771db7accc335161c441e49
masaki8415 <63185909+Masaki8415@users.noreply.github.com>
2021-12-18 12:50:47 +0100

    zwei klausuren test geschrieben und Fehlerbehoben, neue fehler hinzugefügt, damit ich nicht arbeitslos werde

--- src/test/java/de/fhwedel/klausps/controller/restriction/hard/TwoKlausurenSameTimeTest.java
+++ src/test/java/de/fhwedel/klausps/controller/restriction/hard/TwoKlausurenSameTimeTest.java
@@ -420,4 +420,118 @@ class TwoKlausurenSameTimeTest {
     assertEquals(setOfConflictTeilnehmer, h.inConfilictTeilnehmerkreis);
     assertEquals(studends, h.countStudents);
   }
+
+
+
+
+  @Test
+  void twoKlausurenSameTime_ThreeSameTime_two_DiffrentTeilnehmerkreis() {
+
+    TwoKlausurenSameTime h = new TwoKlausurenSameTime(this.dataAccessService,
+        HartesKriterium.ZWEI_KLAUSUREN_GLEICHZEITIG);
+
+    Planungseinheit analysisPL = mock(Planungseinheit.class);
+    Planungseinheit haskelPL = mock(Planungseinheit.class);
+    Planungseinheit dmPL = mock(Planungseinheit.class);
+
+    Pruefung analysis = mock(Pruefung.class);
+    Pruefung haskel = mock(Pruefung.class);
+    Pruefung dm = mock(Pruefung.class);
+
+    Set<Teilnehmerkreis> teilnehmer1 = new HashSet<>();
+    Teilnehmerkreis informatik = getTeilnehmerKreis("Informatik");
+    teilnehmer1.add(informatik);
+
+    Set<Teilnehmerkreis> teilnehmer2 = new HashSet<>();
+    Teilnehmerkreis irgendwasMitMedien = getTeilnehmerKreis("irgendwasMitMedien");
+    teilnehmer2.add(irgendwasMitMedien);
+
+    Set<Teilnehmerkreis> teilnehmer3= new HashSet<>();
+    teilnehmer3.add(irgendwasMitMedien);
+    teilnehmer3.add(informatik);
+
+    when(analysis.getTeilnehmerkreise()).thenReturn(teilnehmer1);
+    when(dm.getTeilnehmerkreise()).thenReturn(teilnehmer2);
+    when(haskel.getTeilnehmerkreise()).thenReturn(teilnehmer3);
+
+
+
+
+//   Set<Planungseinheit> setOfPruefungen = new HashSet<>();
+//   setOfPruefungen.add(analysisPL);
+//   setOfPruefungen.add(dmPL);
+
+
+
+    Set<Pruefung> setOfConflictPruefunge = new HashSet<>();
+    setOfConflictPruefunge.add(dm);
+    setOfConflictPruefunge.add(analysis);
+    setOfConflictPruefunge.add(haskel);
+
+
+    Set<Teilnehmerkreis> setOfConflictTeilnehmerkreis = new HashSet<>();
+    setOfConflictTeilnehmerkreis.add(informatik);
+    setOfConflictTeilnehmerkreis.add(irgendwasMitMedien);
+
+    Set<Teilnehmerkreis> haskelTeilnehmer3 = new HashSet<>();
+    haskelTeilnehmer3.add(informatik);
+    haskelTeilnehmer3.add(irgendwasMitMedien);
+
+    Set<Teilnehmerkreis> analysisTeilnehmer1 = new HashSet<>();
+    analysisTeilnehmer1.add(informatik);
+
+
+    Set<Teilnehmerkreis> dmTeilnehmer2 = new HashSet<>();
+
+    dmTeilnehmer2.add(irgendwasMitMedien);
+
+    int studends = 16;
+
+    Map<Teilnehmerkreis, Integer> teilnehmerCount1 = new HashMap<>();
+    teilnehmerCount1.put(informatik, 8);
+
+    Map<Teilnehmerkreis, Integer> teilnehmerCount2 = new HashMap<>();
+    teilnehmerCount2.put(irgendwasMitMedien, 8);
+
+    Map<Teilnehmerkreis, Integer> teilnehmerCount3 = new HashMap<>();
+    teilnehmerCount3.put(irgendwasMitMedien, 8);
+    teilnehmerCount3.put(informatik, 8);
+
+    when(analysis.getSchaetzungen()).thenReturn(teilnehmerCount1);
+    when(dm.getSchaetzungen()).thenReturn(teilnehmerCount2);
+    when(haskel.getSchaetzungen()).thenReturn(teilnehmerCount3);
+
+    when(analysis.getTeilnehmerkreise()).thenReturn(analysisTeilnehmer1);
+    when(dm.getTeilnehmerkreise()).thenReturn(dmTeilnehmer2);
+    when(haskel.getTeilnehmerkreise()).thenReturn(haskelTeilnehmer3);
+
+
+    LocalDateTime start = LocalDateTime.of(2021, 8, 11, 9, 0);
+
+    when(analysisPL.asPruefung()).thenReturn(analysis);
+    when(haskelPL.asPruefung()).thenReturn(haskel);
+    when(dmPL.asPruefung()).thenReturn(dm);
+    // when(pruefungsperiode.planungseinheitenBetween(start, start.plusMinutes(120))).thenReturn(setOfPruefungen);
+
+
+    ArrayList<Pruefung> listOfPruefungen = new ArrayList<>();
+    listOfPruefungen.add(dm);
+    listOfPruefungen.add(haskel);
+    listOfPruefungen.add(analysis);
+
+
+    when(dataAccessService.getAllPruefungenBetween(any(), any())).thenReturn(listOfPruefungen);
+
+    Duration duration = Duration.ofMinutes(120);
+
+
+    when(haskel.getStartzeitpunkt()).thenReturn(start);
+    when(haskel.getDauer()).thenReturn(duration);
+
+    assertTrue(h.test(haskel));
+
+    assertEquals(setOfConflictPruefunge, h.inConflictROPruefung);
+    assertEquals(setOfConflictTeilnehmerkreis, h.inConfilictTeilnehmerkreis);
+    assertEquals(studends, h.countStudents);
+  }
 }
