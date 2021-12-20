28451c25abfd0c8dc2e54e6db69cbf4f1322f49f
Miwo <inf104162@fh-wedel.de>
2021-12-20 13:50:39 +0100

    adjusted required changes.

--- src/main/java/de/fhwedel/klausps/controller/restriction/soft/AnzahlPruefungProWoche.java
+++ src/main/java/de/fhwedel/klausps/controller/restriction/soft/AnzahlPruefungProWoche.java
@@ -8,14 +8,20 @@ import de.fhwedel.klausps.controller.kriterium.KriteriumsAnalyse;
 import de.fhwedel.klausps.controller.services.DataAccessService;
 import de.fhwedel.klausps.controller.services.ServiceProvider;
 import de.fhwedel.klausps.model.api.Block;
+import de.fhwedel.klausps.model.api.Planungseinheit;
 import de.fhwedel.klausps.model.api.Pruefung;
 import de.fhwedel.klausps.model.api.Teilnehmerkreis;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AnzahlPruefungProWoche extends WeicheRestriktion implements Predicate<Pruefung> {
 
@@ -105,6 +111,100 @@ public class AnzahlPruefungProWoche extends WeicheRestriktion implements Predica
     
     return Optional.of(new KriteriumsAnalyse(conflictedRoPruefungen,
         ANZAHL_PRUEFUNGEN_PRO_WOCHE, conflictedTeilnehmerkreis, affected));
+  }
+
+  /**
+   * Calculates the number of students of the passed Set of pruefungen. It considera als the
+   * duplicates Teilnehmerkreise.
+   *
+   * @param pruefungen Set of Pruefung.
+   * @return number of affected students.
+   */
+  private int numberAffectedStudents(Set<Pruefung> pruefungen) {
+    return pruefungen.stream()
+        .flatMap(pruefung -> pruefung.getSchaetzungen().entrySet().stream())
+        .filter(distinctByKey(Entry::getKey)).mapToInt(Entry::getValue).sum();
+  }
+
+  /**
+   * Can't use normal distinct, when there are different Teilnhmerkreis with different Schätzung.
+   * https://stackoverflow.com/questions/23699371/java-8-distinct-by-property
+   *
+   * @param keyExtractor filter
+   * @param <T>          Type.
+   * @return Predicate.
+   */
+  private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+    Set<Object> seen = ConcurrentHashMap.newKeySet();
+    return t -> seen.add(keyExtractor.apply(t));
+  }
+
+
+  /**
+   * Filters the block siblings of the passed pruefung. When the passed pruefung doesn't belong to
+   * any block, the same set will be returned.
+   *
+   * @param pruefung   Pruefung to filter its siblings.
+   * @param pruefungen Set with Pruefung to filter.
+   * @return filtered Set without the sibbling of pruefung, but it contains still the passed
+   * pruefung.
+   * @assert pruefungen must contains pruefung
+   */
+  private Set<Pruefung> filterSiblingsOfPruefung(Pruefung pruefung,
+      Set<Pruefung> pruefungen) {
+    assert pruefungen.contains(pruefung);
+    Optional<Block> blockOpt = dataAccessService.getBlockTo(pruefung);
+
+    if (blockOpt.isEmpty()) {
+      return pruefungen;
+    }
+
+    Set<Planungseinheit> accPlanungseinheiten = getAccumulatedPlanungseinheiten(
+        pruefungen);
+    accPlanungseinheiten.remove(blockOpt.get());
+    Set<Pruefung> filtered = getExtractedPruefungen(
+        accPlanungseinheiten);
+    filtered.add(pruefung);
+    return filtered;
+  }
+
+  /**
+   * If there is block, it will give us the siblings exam inside the block and not the block
+   * itself.
+   *
+   * @param planungseinheiten blocks or exams
+   * @return All Pruefungen
+   */
+  private Set<Pruefung> getExtractedPruefungen(
+      Set<Planungseinheit> planungseinheiten) {
+    Stream<Pruefung> fromBlock = planungseinheiten.stream().filter(Planungseinheit::isBlock)
+        .flatMap(block -> block.asBlock().getPruefungen().stream());
+
+    Stream<Pruefung> withoutBlock = planungseinheiten.stream()
+        .filter(planungseinheit -> !planungseinheit.isBlock())
+        .map(Planungseinheit::asPruefung);
+
+    return Stream.concat(fromBlock, withoutBlock).collect(Collectors.toSet());
+  }
+
+  /**
+   * Accumulates the exams, the set only contains distinct Planungseinheiten. When a pruefung is in
+   * a block the returned Set contains the block of the pruefung and not the exam itself.
+   *
+   * @param pruefungen Pruefungen.
+   * @return Accumlated Planungseinheiten.
+   */
+  private Set<Planungseinheit> getAccumulatedPlanungseinheiten(Set<Pruefung> pruefungen) {
+    Set<Planungseinheit> planungseinheiten = new HashSet<>();
+    for (Pruefung p : pruefungen) {
+      Optional<Block> blockOpt = dataAccessService.getBlockTo(p);
+      if (blockOpt.isPresent()) {
+        planungseinheiten.add(blockOpt.get());
+      } else {
+        planungseinheiten.add(p);
+      }
+    }
+    return planungseinheiten;
   }
 
 }
