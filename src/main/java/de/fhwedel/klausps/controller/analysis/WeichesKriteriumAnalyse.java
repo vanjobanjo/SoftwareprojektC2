e3f53ce5819615fd3ef9bf13760f79455fbc8f22
Miwo <inf104162@fh-wedel.de>
2021-12-29 13:44:16 +0100

    delta scoring.

--- src/main/java/de/fhwedel/klausps/controller/analysis/WeichesKriteriumAnalyse.java
+++ src/main/java/de/fhwedel/klausps/controller/analysis/WeichesKriteriumAnalyse.java
@@ -13,14 +13,31 @@ public class WeichesKriteriumAnalyse {
   WeichesKriterium kriterium;
   Set<Teilnehmerkreis> affectedTeilnehmerKreise;
   Integer amountAffectedStudents;
+  //Verursachte Scoring Änderung durch dieses verletzte Kriterium.
+  //Um das Scoring der Klausur zu bestimmen, muss die Summe der deltaScoring aus der Liste ergeben
+  //das Scoring der Pruefung
 
+  Integer deltaScoring;
 
+  /**
+   * @deprecated delta scoring
+   */
+  @Deprecated
   public WeichesKriteriumAnalyse(Set<Pruefung> causingPruefungen, WeichesKriterium kriterium,
       Set<Teilnehmerkreis> affectedTeilnehmerKreise, int affectedStudents) {
     this.causingPruefungen = causingPruefungen;
     this.kriterium = kriterium;
     this.affectedTeilnehmerKreise = affectedTeilnehmerKreise;
     this.amountAffectedStudents = affectedStudents;
+  }
+
+  public WeichesKriteriumAnalyse(Set<Pruefung> causingPruefungen, WeichesKriterium kriterium,
+      Set<Teilnehmerkreis> affectedTeilnehmerKreise, int affectedStudents, int scoring) {
+    this.causingPruefungen = causingPruefungen;
+    this.kriterium = kriterium;
+    this.affectedTeilnehmerKreise = affectedTeilnehmerKreise;
+    this.amountAffectedStudents = affectedStudents;
+    this.deltaScoring = scoring;
   }
 
   public Set<Pruefung> getCausingPruefungen() {
