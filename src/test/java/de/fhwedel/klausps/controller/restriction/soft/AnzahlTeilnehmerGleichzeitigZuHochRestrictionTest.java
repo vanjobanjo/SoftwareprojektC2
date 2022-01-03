ffc3f542d2f92200d2d943e3aa999077e3c764ee
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2022-01-03 09:53:27 +0100

    #139 restriction is not violated if the limit of Teilnehmer is not exceeded

--- src/test/java/de/fhwedel/klausps/controller/restriction/soft/AnzahlTeilnehmerGleichzeitigZuHochRestrictionTest.java
+++ src/test/java/de/fhwedel/klausps/controller/restriction/soft/AnzahlTeilnehmerGleichzeitigZuHochRestrictionTest.java
@@ -1,6 +1,7 @@
 package de.fhwedel.klausps.controller.restriction.soft;
 
 import static de.fhwedel.klausps.controller.util.TestUtils.getRandomPlannedPruefung;
+import static de.fhwedel.klausps.controller.util.TestUtils.getRandomPlannedPruefungen;
 import static de.fhwedel.klausps.controller.util.TestUtils.getRandomTeilnehmerkreis;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,7 +10,13 @@ import static org.mockito.Mockito.when;
 
 import de.fhwedel.klausps.controller.exceptions.IllegalTimeSpanException;
 import de.fhwedel.klausps.controller.services.DataAccessService;
+import de.fhwedel.klausps.model.api.Block;
+import de.fhwedel.klausps.model.api.Blocktyp;
 import de.fhwedel.klausps.model.api.Pruefung;
+import de.fhwedel.klausps.model.api.Pruefungsperiode;
+import de.fhwedel.klausps.model.impl.BlockImpl;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,12 +36,12 @@ class AnzahlTeilnehmerGleichzeitigZuHochRestrictionTest {
 
   /*
    *   Grenzfälle:
-   * O Grade so viele Personen, dass die Restriktion nicht verletzt wird (Klausur / Block)
+   * x Grade so viele Personen, dass die Restriktion nicht verletzt wird (Klausur / Block)
    * O Genau so viele Personen, dass die Restriktion minimal verletzt wird (Klausur / Block)
    * O Genau die betroffenen Teilnehmerkreise sind enthalten (Klausur / Block)
    * O Die Anzahl der betroffenen Studenten ist korrekt (Klausur / Block)
-   * O Das minimal mögliche scoring
-   * O Nächst höheres scoring, ...
+   * O Das minimal moegliche scoring
+   * O Naechst hoeheres scoring, ...
    */
 
   @Test
@@ -61,6 +68,51 @@ class AnzahlTeilnehmerGleichzeitigZuHochRestrictionTest {
     when(dataAccessService.getBlockTo(any(Pruefung.class))).thenReturn(Optional.empty());
 
     assertThat(deviceUnderTest.evaluate(pruefung)).isNotPresent();
+  }
+
+  @Test
+  void restrictionNotViolatedWhenSlightlyLessStudentsThanPermitted_multiplePruefungen()
+      throws IllegalTimeSpanException {
+    List<Pruefung> pruefungen = get3PruefungenWithTotal199Students();
+
+    when(dataAccessService.getAllPruefungenBetween(any(), any())).thenReturn(
+        Set.copyOf(pruefungen));
+    when(dataAccessService.getBlockTo(any(Pruefung.class))).thenReturn(Optional.empty());
+
+    assertThat(deviceUnderTest.evaluate(pruefungen.get(0))).isNotPresent();
+  }
+
+  private List<Pruefung> get3PruefungenWithTotal199Students() {
+    LocalDateTime startTime = LocalDateTime.of(2012, 12, 31, 1, 30);
+    List<Pruefung> pruefungen = getRandomPlannedPruefungen(1L, 3);
+    for (Pruefung pruefung : pruefungen) {
+      pruefung.setStartzeitpunkt(startTime);
+    }
+    pruefungen.get(0).addTeilnehmerkreis(getRandomTeilnehmerkreis(1L), 111);
+    pruefungen.get(1).addTeilnehmerkreis(getRandomTeilnehmerkreis(2L), 42);
+    pruefungen.get(2).addTeilnehmerkreis(getRandomTeilnehmerkreis(3L), 46);
+    return pruefungen;
+  }
+
+  @Test
+  void restrictionNotViolatedWhenSlightlyLessStudentsThanPermitted_multiplePruefungenInOneBlock()
+      throws IllegalTimeSpanException {
+    List<Pruefung> pruefungen = get3PruefungenWithTotal199Students();
+    Block block = getBlockWith(pruefungen);
+
+    when(dataAccessService.getAllPruefungenBetween(any(), any())).thenReturn(
+        Set.copyOf(pruefungen));
+    when(dataAccessService.getBlockTo(any(Pruefung.class))).thenReturn(Optional.of(block));
+
+    assertThat(deviceUnderTest.evaluate(pruefungen.get(0))).isNotPresent();
+  }
+
+  private Block getBlockWith(Iterable<Pruefung> pruefungen) {
+    Block block = new BlockImpl(mock(Pruefungsperiode.class), "name", Blocktyp.PARALLEL);
+    for (Pruefung pruefung : pruefungen) {
+      block.addPruefung(pruefung);
+    }
+    return block;
   }
 
 }
