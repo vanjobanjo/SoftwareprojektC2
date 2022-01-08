fe2f4155cb93aefea29af34640119de78a79f8da
Miwo <inf104162@fh-wedel.de>
2022-01-08 14:33:38 +0100

    exporter

--- src/main/java/de/fhwedel/klausps/controller/Controller.java
+++ src/main/java/de/fhwedel/klausps/controller/Controller.java
@@ -20,6 +20,8 @@ import de.fhwedel.klausps.model.api.Blocktyp;
 import de.fhwedel.klausps.model.api.Semester;
 import de.fhwedel.klausps.model.api.Semestertyp;
 import de.fhwedel.klausps.model.api.Teilnehmerkreis;
+import de.fhwedel.klausps.model.api.exporter.ExportException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -282,6 +284,7 @@ public class Controller implements InterfaceController {
     return scheduleService.schedulePruefung(pruefung, startTermin);
   }
 
+
   @Override
   public ReadOnlyPlanungseinheit addPruefer(ReadOnlyPruefung pruefung, String kuerzel)
       throws NoPruefungsPeriodeDefinedException {
@@ -306,6 +309,7 @@ public class Controller implements InterfaceController {
     checkNoPruefungDefined();
 
     return this.scheduleService.addTeilnehmerkreis(pruefung,teilnehmerkreis,schaetzung);
+
   }
 
   @Override
@@ -400,7 +404,14 @@ public class Controller implements InterfaceController {
   public void exportPeriode(Path path, ExportTyp typ) throws NoPruefungsPeriodeDefinedException {
     noNullParameters(path, typ);
     checkNoPruefungDefined();
-    throw new IllegalStateException("Not implemented yet!");
+    try {
+      ioService.exportPeriode(path, typ);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ExportException e) {
+      e.printStackTrace();
+    }
+    //TODO kann nach nächstem deploy weg.
   }
 
   @Override
--- src/main/java/de/fhwedel/klausps/controller/services/IOService.java
+++ src/main/java/de/fhwedel/klausps/controller/services/IOService.java
@@ -1,7 +1,16 @@
 package de.fhwedel.klausps.controller.services;
 
+import de.fhwedel.klausps.controller.export.ExportTyp;
+import de.fhwedel.klausps.model.api.Pruefungsperiode;
 import de.fhwedel.klausps.model.api.Semester;
+import de.fhwedel.klausps.model.api.exporter.ExportException;
+import de.fhwedel.klausps.model.api.exporter.Exporter;
 import de.fhwedel.klausps.model.impl.PruefungsperiodeImpl;
+import de.fhwedel.klausps.model.impl.exporter.CSVExporterImpl;
+import de.fhwedel.klausps.model.impl.exporter.KlausPSExporterImpl;
+import de.fhwedel.klausps.model.impl.exporter.PDFExporterImpl;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.time.LocalDate;
 
 public class IOService {
@@ -16,5 +25,17 @@ public class IOService {
       Semester semester, LocalDate start, LocalDate end, int kapazitaet) {
     dataAccessService.setPruefungsperiode(
         new PruefungsperiodeImpl(semester, start, end, kapazitaet));
+  }
+
+  public void exportPeriode(Path path, ExportTyp typ) throws IOException, ExportException {
+    Exporter export;
+    Pruefungsperiode periode = dataAccessService.getPruefungsperiode();
+    switch (typ) {
+      case CSV -> export = new CSVExporterImpl(periode);
+      case PDF -> export = new PDFExporterImpl(periode);
+      case INTERN -> export = new KlausPSExporterImpl(periode);
+      default -> throw new IllegalStateException("Unexpected value: " + typ);
+    }
+    export.exportTo(path);
   }
 }
