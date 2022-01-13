188f0ed0f22f881f084a48a321f51d671347f8fe
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2022-01-13 11:45:06 +0100

    reactivate Restrictions registration

--- src/main/java/de/fhwedel/klausps/controller/services/RestrictionFactory.java
+++ src/main/java/de/fhwedel/klausps/controller/services/RestrictionFactory.java
@@ -3,6 +3,7 @@ package de.fhwedel.klausps.controller.services;
 
 import de.fhwedel.klausps.controller.restriction.hard.HarteRestriktion;
 import de.fhwedel.klausps.controller.restriction.hard.TwoKlausurenSameTime;
+import de.fhwedel.klausps.controller.restriction.soft.AnzahlPruefungProWocheTeilnehmerkreis;
 import de.fhwedel.klausps.controller.restriction.soft.AnzahlPruefungenGleichzeitigRestriktion;
 import de.fhwedel.klausps.controller.restriction.soft.AnzahlTeilnehmerGleichzeitigZuHochRestriction;
 import de.fhwedel.klausps.controller.restriction.soft.FreierTagZwischenPruefungen;
@@ -11,6 +12,7 @@ import de.fhwedel.klausps.controller.restriction.soft.MehrePruefungenAmTag;
 import de.fhwedel.klausps.controller.restriction.soft.PruefungenMitVielenAmAnfangRestriction;
 import de.fhwedel.klausps.controller.restriction.soft.UniformeZeitslots;
 import de.fhwedel.klausps.controller.restriction.soft.WeicheRestriktion;
+import de.fhwedel.klausps.controller.restriction.soft.WocheVierFuerMaster;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,16 +31,15 @@ public class RestrictionFactory {
   private static void registerSoftCriteria(RestrictionService service) {
     if (softRestrictions.isEmpty()) {
       softRestrictions.addAll(Set.of(
-          // TODO restriktionen aktivieren wenn wieder funktionsfähig
           // new AnzahlPruefungProWoche(), deprecated.
-          //new AnzahlPruefungProWocheTeilnehmerkreis(),
+          new AnzahlPruefungProWocheTeilnehmerkreis(),
           new AnzahlPruefungenGleichzeitigRestriktion(),
           new AnzahlTeilnehmerGleichzeitigZuHochRestriction(),
           new FreierTagZwischenPruefungen(),
           new KeineKlausurAmSonntag(),
           new UniformeZeitslots(),
           new MehrePruefungenAmTag(),
-          //new WocheVierFuerMaster(),
+          new WocheVierFuerMaster(),
           new PruefungenMitVielenAmAnfangRestriction()
       ));
     }
