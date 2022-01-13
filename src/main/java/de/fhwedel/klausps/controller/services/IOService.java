ab2d3b54bdbdfd45d18b52c25c09c9c6f823c736
vanjobanjo <44374624+vanjobanjo@users.noreply.github.com>
2022-01-13 16:06:25 +0100

    resolves #215

--- src/main/java/de/fhwedel/klausps/controller/services/IOService.java
+++ src/main/java/de/fhwedel/klausps/controller/services/IOService.java
@@ -1,14 +1,19 @@
 package de.fhwedel.klausps.controller.services;
 
+import static de.fhwedel.klausps.controller.util.ParameterUtil.noNullParameters;
+
 import de.fhwedel.klausps.controller.export.ExportTyp;
 import de.fhwedel.klausps.model.api.Pruefungsperiode;
 import de.fhwedel.klausps.model.api.Semester;
 import de.fhwedel.klausps.model.api.exporter.ExportException;
 import de.fhwedel.klausps.model.api.exporter.Exporter;
+import de.fhwedel.klausps.model.api.importer.ImportException;
+import de.fhwedel.klausps.model.api.importer.Importer;
 import de.fhwedel.klausps.model.impl.PruefungsperiodeImpl;
 import de.fhwedel.klausps.model.impl.exporter.CSVExporterImpl;
 import de.fhwedel.klausps.model.impl.exporter.KlausPSExporterImpl;
 import de.fhwedel.klausps.model.impl.exporter.PDFExporterImpl;
+import de.fhwedel.klausps.model.impl.importer.KlausPSImporterImpl;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDate;
@@ -28,6 +33,7 @@ public class IOService {
   }
 
   public void exportPeriode(Path path, ExportTyp typ) throws IOException, ExportException {
+    noNullParameters(path, typ);
     Exporter export;
     Pruefungsperiode periode = dataAccessService.getPruefungsperiode();
     switch (typ) {
@@ -38,4 +44,12 @@ public class IOService {
     }
     export.exportTo(path);
   }
+
+  public void importPeriode(Path path) throws ImportException, IOException {
+    noNullParameters(path);
+    Importer importer = new KlausPSImporterImpl();
+    Pruefungsperiode importedPeriode = importer.importFrom(path);
+    dataAccessService.setPruefungsperiode(importedPeriode);
+  }
+
 }
