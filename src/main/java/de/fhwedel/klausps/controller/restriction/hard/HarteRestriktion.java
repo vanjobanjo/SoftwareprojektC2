3c64a19e012355dd9b15d650eb49f9e38efe014e
Masaki8415 <63185909+Masaki8415@users.noreply.github.com>
2022-01-18 12:40:15 +0100

    Kommentare hinzugefügt

--- src/main/java/de/fhwedel/klausps/controller/restriction/hard/HarteRestriktion.java
+++ src/main/java/de/fhwedel/klausps/controller/restriction/hard/HarteRestriktion.java
@@ -12,22 +12,66 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Abstracte Klasse fuer die Harten Kriteren. Hier werden die einheitlichen Methoden bestimmt, die
+ * gebraucht werden um auf eine Hartes Kriterum getestet zu werden
+ */
+
 public abstract class HarteRestriktion extends Restriktion {
 
+  /**
+   * hardRestriction, hier wird das HarteKriterum, um welches es sich handelt gespeichert
+   */
   protected final HartesKriterium hardRestriction;
+  /**
+   * Zum speichern des DataAccessService
+   */
   protected DataAccessService dataAccessService;
 
+  /**
+   * Super Konstruker, welcher sich den DataAccessService speichert und das HarteKriterum
+   *
+   * @param dataAccessService der DataAccessService, der gespeichert wird
+   * @param kriterium         das Kriterum, welches getestet werden soll
+   */
   HarteRestriktion(DataAccessService dataAccessService, HartesKriterium kriterium) {
     this.hardRestriction = kriterium;
     this.dataAccessService = dataAccessService;
   }
 
+  /**
+   * Dise Methode wertet zu der Übergebenen Pruefung das HarteKriterum aus. Und gibt ein Optional
+   * von einer Harten KriterumsAnalyse zurück.
+   *
+   * @param pruefung die Pruefung die neu hinzugekommen ist, und mit der das HarteKritum getestet
+   *                 werden soll
+   * @return ein Optional, welches entweder leer ist, wenn das HarteKriterum nicht zutrifft, oder
+   * eine HarteKrteriumsAnalyse, mit den Klausuren und Teilnehmerkreisen, welche gegen das Kriterum
+   * verstößt
+   */
   public abstract Optional<HartesKriteriumAnalyse> evaluate(Pruefung pruefung)
       throws NoPruefungsPeriodeDefinedException;
 
+  /**
+   * //TODO nochmal gucken, ob dieser Kommentar stimmt Methode, um alle Pruefungen, zu bekommen, an
+   * der die übergebene Planungseinheit, nicht stattfinden soll
+   *
+   * @param planungseinheitToCheckFor die Planungseinheit, welche zu testen ist, wo sie nicht
+   *                                  stattfinden darf
+   * @return ein Set von Pruefungen, wo die Planungseinheit nicht stattfinden darf
+   */
   public abstract Set<Pruefung> getAllPotentialConflictingPruefungenWith(
       Planungseinheit planungseinheitToCheckFor);
 
+  /**
+   * Methode, die testet, ob der Übergebene Zeitpunkt und mit der Planungseinheit das HarteKriterum
+   * missachtet
+   *
+   * @param startTime       der Zeitpunkt der mit der Planungseinheit getestet werden soll
+   * @param planungseinheit die Planungseinheit, für die getestet werden soll
+   * @return true, wenn die Planungseinheit, an den Übergebenen Zeitpunkt stattfinden darf
+   * @throws NoPruefungsPeriodeDefinedException, wenn keien Pruefungsperiode Definiert ist
+   */
   public abstract boolean wouldBeHardConflictAt(LocalDateTime startTime,
       Planungseinheit planungseinheit)
       throws NoPruefungsPeriodeDefinedException;
