ad9c006a5436265f9ae634151d9f2735a8e8da22
Miwo <inf104162@fh-wedel.de>
2022-02-16 12:07:19 +0100

    Alle Integrationstest wurden geschriebemn

--- src/test/java/integrationTests/steps/createBlockSteps.java
+++ src/test/java/integrationTests/steps/createBlockSteps.java
@@ -4,6 +4,7 @@ import static integrationTests.steps.BaseSteps.state;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import de.fhwedel.klausps.controller.api.builders.PruefungDTOBuilder;
+import de.fhwedel.klausps.controller.api.view_dto.ReadOnlyBlock;
 import de.fhwedel.klausps.controller.api.view_dto.ReadOnlyPruefung;
 import de.fhwedel.klausps.controller.exceptions.HartesKriteriumException;
 import de.fhwedel.klausps.controller.exceptions.NoPruefungsPeriodeDefinedException;
@@ -16,11 +17,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Collections;
-import org.junit.AssumptionViolatedException;
 
 public class createBlockSteps {
 
-  @Wenn("ich einen Block mit den Pruefungen {string} und {string} erstelle")
+  private static final String RESULT_BLOCK = "Block";
+  private static final String RESULT_EXCEPTION = "Eine der übergebenen Prüfungen ist geplant.";
+
+  @Wenn("ich einen Block mit den geplanten Pruefungen {string} und {string} erstelle")
   public void erstelleBlockMitPruefungen(String pruefung1, String pruefung2)
       throws NoPruefungsPeriodeDefinedException {
     ReadOnlyPruefung rp1 = new PruefungDTOBuilder().withPruefungsNummer(pruefung1)
@@ -30,7 +33,7 @@ public class createBlockSteps {
     try {
       state.controller.createBlock("Hallo", Blocktyp.SEQUENTIAL, rp1, rp2);
     } catch (IllegalArgumentException e) {
-      state.results.put("Eine der übergebenen Prüfungen ist geplant.",
+      state.results.put(RESULT_EXCEPTION,
           e);
     }
   }
@@ -51,49 +54,122 @@ public class createBlockSteps {
   }
 
   @Angenommen("es existieren die ungeplanten Klausuren {string} und {string}")
-  public void esExistierenDieUngeplantenKlausurenUnd(String pruefung1, String pruefung2) {
-    throw new AssumptionViolatedException("not implemented");
-  }
+  public void esExistierenDieUngeplantenKlausurenUnd(String pruefung1, String pruefung2)
+      throws NoPruefungsPeriodeDefinedException {
+    state.controller.createPruefung(pruefung1, pruefung1, pruefung1,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
 
-  @Angenommen("es existieren die ungeplanten Pruefungen {string} und {string}")
-  public void esExistierenDieUngeplantenPruefungenUnd(String pruefung1, String pruefung2) {
-    throw new AssumptionViolatedException("not implemented");
+    state.controller.createPruefung(pruefung2, pruefung2, pruefung2,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
   }
 
   @Angenommen("es existiert die geplante Pruefung {string} und die ungeplante Pruefung {string}")
   public void esExistiertDieGeplantePruefungUndDieUngeplantePruefung(String pruefung1,
-      String pruefung2) {
-    throw new AssumptionViolatedException("not implemented");
+      String pruefung2) throws NoPruefungsPeriodeDefinedException, HartesKriteriumException {
+    ReadOnlyPruefung rp1 = state.controller.createPruefung(pruefung1, pruefung1, pruefung1,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
+
+    ReadOnlyPruefung rp2 = state.controller.createPruefung(pruefung2, pruefung2, pruefung2,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
+    LocalDate.of(2022, 1, 31);
+    state.controller.schedulePruefung(rp1, LocalDate.of(2022, 1, 31).atTime(LocalTime.MAX));
   }
 
   @Angenommen("es existiert die ungeplante Pruefung {string}")
-  public void esExistiertDieUngeplantePruefung(String pruefung) {
-    throw new AssumptionViolatedException("not implemented");
+  public void esExistiertDieUngeplantePruefung(String pruefung1)
+      throws NoPruefungsPeriodeDefinedException {
+    state.controller.createPruefung(pruefung1, pruefung1, pruefung1,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
   }
 
   @Wenn("ich einen Block mit der Pruefung {string} erstelle")
-  public void ichEinenBlockMitDerPruefungErstelle(String pruefung) {
-    throw new AssumptionViolatedException("not implemented");
+  public void ichEinenBlockMitDerPruefungErstelle(String pruefung1)
+      throws NoPruefungsPeriodeDefinedException {
+    ReadOnlyPruefung rp1 = new PruefungDTOBuilder().withPruefungsNummer(pruefung1)
+        .withPruefungsName(pruefung1).build();
+    ReadOnlyBlock result;
+    result = state.controller.createBlock("Hallo", Blocktyp.SEQUENTIAL, rp1);
+    state.results.put(RESULT_BLOCK, result);
   }
 
   @Dann("erhalte ich einen Block mit der Pruefung {string}")
-  public void erhalteIchEinenBlockMitDerPruefung(String pruefung) {
-    throw new AssumptionViolatedException("not implemented");
+  public void erhalteIchEinenBlockMitDerPruefung(String pruefung1) {
+    ReadOnlyBlock result = (ReadOnlyBlock) state.results.get(RESULT_BLOCK);
+    assertThat(result.getROPruefungen().stream()
+        .anyMatch(pruefung -> pruefung.getName().equals(pruefung1))).isTrue();
   }
 
   @Angenommen("es existiert die geplante Pruefung {string}")
-  public void esExistiertDieGeplantePruefung(String pruefung) {
-    throw new AssumptionViolatedException("not implemented");
+  public void esExistiertDieGeplantePruefung(String pruefung1)
+      throws NoPruefungsPeriodeDefinedException, HartesKriteriumException {
+    ReadOnlyPruefung rp1 = state.controller.createPruefung(pruefung1, pruefung1, pruefung1,
+        Collections.emptySet(),
+        Duration.ofMinutes(120), Collections.emptyMap());
+    LocalDate.of(2022, 1, 31);
+    state.controller.schedulePruefung(rp1, LocalDate.of(2022, 1, 31).atTime(LocalTime.MAX));
   }
 
   @Dann("erhalte ich einen Block mit den Pruefungen {string} und {string}")
   public void erhalteIchEinenBlockMitDenPruefungenUnd(String pruefung1, String pruefung2) {
+    ReadOnlyBlock result = (ReadOnlyBlock) state.results.get(RESULT_BLOCK);
+    assertThat(result.getROPruefungen().stream()
+        .anyMatch(pruefung -> pruefung.getName().equals(pruefung1))).isTrue();
+    assertThat(result.getROPruefungen().stream()
+        .anyMatch(pruefung -> pruefung.getName().equals(pruefung2))).isTrue();
   }
-
 
   @Dann("erhalte ich ein Fehlermeldung Pruefungen sind geplant")
   public void erhalteIchEinFehlermeldungPruefungenSindGeplant() {
-    Object obj = state.results.get("Eine der übergebenen Prüfungen ist geplant.");
+    Object obj = state.results.get(RESULT_EXCEPTION);
     assertThat(obj).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Wenn("ich einen Block mit den ungeplanten Pruefungen {string} und {string} erstelle")
+  public void ichEinenBlockMitDenUngeplantenPruefungenUndErstelle(String pruefung1,
+      String pruefung2) throws NoPruefungsPeriodeDefinedException {
+
+    ReadOnlyPruefung rp1 = new PruefungDTOBuilder().withPruefungsNummer(pruefung1)
+        .withPruefungsName(pruefung1).build();
+    ReadOnlyPruefung rp2 = new PruefungDTOBuilder().withPruefungsNummer(pruefung2)
+        .withPruefungsName(pruefung2).build();
+
+    ReadOnlyBlock result;
+
+    result = state.controller.createBlock("Hallo", Blocktyp.SEQUENTIAL, rp1, rp2);
+    state.results.put(RESULT_BLOCK, result);
+  }
+
+  @Wenn("ich einen gemischten Block mit der geplanten Pruefung {string} und ungeplanten {string} erstelle")
+  public void ichEinenGemischtenBlockMitDerGeplantenPruefungUndUngeplantenErstelle(String pruefung1,
+      String pruefung2) {
+
+    ReadOnlyPruefung rp1 = new PruefungDTOBuilder().withPruefungsNummer(pruefung1)
+        .withPruefungsName(pruefung1).withStartZeitpunkt(LocalDateTime.MIN).build();
+    ReadOnlyPruefung rp2 = new PruefungDTOBuilder().withPruefungsNummer(pruefung2)
+        .withPruefungsName(pruefung2).build();
+    try {
+      state.controller.createBlock("Hallo", Blocktyp.SEQUENTIAL, rp1, rp2);
+    } catch (IllegalArgumentException | NoPruefungsPeriodeDefinedException e) {
+      state.results.put(RESULT_EXCEPTION,
+          e);
+    }
+  }
+
+  @Wenn("ich einen Block mit der geplanten Pruefung {string} erstelle")
+  public void ichEinenBlockMitDerGeplantenPruefungErstelle(String pruefung1) {
+
+    ReadOnlyPruefung rp1 = new PruefungDTOBuilder().withPruefungsNummer(pruefung1)
+        .withPruefungsName(pruefung1).withStartZeitpunkt(LocalDateTime.MIN).build();
+    try {
+      state.controller.createBlock("Hallo", Blocktyp.SEQUENTIAL, rp1);
+    } catch (IllegalArgumentException | NoPruefungsPeriodeDefinedException e) {
+      state.results.put(RESULT_EXCEPTION,
+          e);
+    }
   }
 }
--- src/test/resources/features/createBlock.feature
+++ src/test/resources/features/createBlock.feature
@@ -4,40 +4,46 @@ Funktionalität: Als Planende*r moechte ich Bloecke erstellen koennen
   Szenario: Ich erstelle erfolgreich einen Block mit geplanten Klausuren, die sich eigentlich ausschließen
     Angenommen es existiert eine Pruefungsperiode
     Und es existieren die geplanten Pruefungen "Analysis" und "Diskrete Mathematik"
-    Wenn ich einen Block mit den Pruefungen "Analysis" und "Diskrete Mathematik" erstelle
+    Wenn ich einen Block mit den geplanten Pruefungen "Analysis" und "Diskrete Mathematik" erstelle
     Dann erhalte ich ein Fehlermeldung Pruefungen sind geplant
 
 
   Szenario: Ich erstelle erfolgreich einen Block mit geplanten Klausuren, die sich nicht ausschließen
-    Angenommen es existieren die geplanten Pruefungen "AuD" und "foop"
-    Wenn ich einen Block mit den Pruefungen "AuD" und "foop" erstelle
-    Dann erhalte ich einen Block mit den Pruefungen "AuD" und "foop"
+    Angenommen es existiert eine Pruefungsperiode
+    Und es existieren die geplanten Pruefungen "AuD" und "foop"
+    Wenn ich einen Block mit den geplanten Pruefungen "AuD" und "foop" erstelle
+    Dann erhalte ich ein Fehlermeldung Pruefungen sind geplant
 
 
- Szenario: Ich erstelle erfolgreich einen Block mit ungeplanten Klausuren, die sich eigentlich ausschließen
-    Angenommen es existieren die ungeplanten Klausuren "Analysis" und "Diskrete Mathematik"
-    Wenn ich einen Block mit den Pruefungen "Analysis" und "Diskrete Mathematik" erstelle
+  Szenario: Ich erstelle erfolgreich einen Block mit ungeplanten Klausuren, die sich eigentlich ausschließen
+    Angenommen es existiert eine Pruefungsperiode
+    Und es existieren die ungeplanten Klausuren "Analysis" und "Diskrete Mathematik"
+    Wenn ich einen Block mit den ungeplanten Pruefungen "Analysis" und "Diskrete Mathematik" erstelle
     Dann erhalte ich einen Block mit den Pruefungen "Analysis" und "Diskrete Mathematik"
 
 
   Szenario: Ich erstelle erfolgreich einen Block mit ungeplanten Klausuren, die sich nicht ausschließen
-    Angenommen es existieren die ungeplanten Pruefungen "AuD" und "foop"
-    Wenn ich einen Block mit den Pruefungen "AuD" und "foop" erstelle
+    Angenommen es existiert eine Pruefungsperiode
+    Und es existieren die ungeplanten Klausuren "AuD" und "foop"
+    Wenn ich einen Block mit den ungeplanten Pruefungen "AuD" und "foop" erstelle
     Dann erhalte ich einen Block mit den Pruefungen "AuD" und "foop"
 
 
   Szenario: Ich moechte einen Block mit ungeplanten und geplanten Pruefungen erstellen
-    Angenommen es existiert die geplante Pruefung "Digitaltechnik" und die ungeplante Pruefung "Rechnernetze"
-    Wenn ich einen Block mit den Pruefungen "Digitaltechnik" und "Rechnerstrukturen" erstelle
-    Dann erhalte ich ein Fehlermeldung vom Model
+    Angenommen es existiert eine Pruefungsperiode
+    Und es existiert die geplante Pruefung "Digitaltechnik" und die ungeplante Pruefung "Rechnernetze"
+    Wenn ich einen gemischten Block mit der geplanten Pruefung "Digitaltechnik" und ungeplanten "Rechnerstrukturen" erstelle
+    Dann erhalte ich ein Fehlermeldung Pruefungen sind geplant
 
 
-    Szenario: Ich erstelle einen Block mit nur einer ungeplanten Pruefung
-      Angenommen es existiert die ungeplante Pruefung "Echtzeitsysteme"
-      Wenn ich einen Block mit der Pruefung "Echtzeitsysteme" erstelle
-      Dann erhalte ich einen Block mit der Pruefung "Echtzeitsysteme"
+  Szenario: Ich erstelle einen Block mit nur einer ungeplanten Pruefung
+    Angenommen es existiert eine Pruefungsperiode
+    Angenommen es existiert die ungeplante Pruefung "Echtzeitsysteme"
+    Wenn ich einen Block mit der Pruefung "Echtzeitsysteme" erstelle
+    Dann erhalte ich einen Block mit der Pruefung "Echtzeitsysteme"
 
   Szenario: Ich erstelle einen Block mit nur einer geplanten Pruefung
-      Angenommen es existiert die geplante Pruefung "Echtzeitsysteme"
-      Wenn ich einen Block mit der Pruefung "Echtzeitsysteme" erstelle
-      Dann erhalte ich einen Block mit der Pruefung "Echtzeitsysteme"
+    Angenommen es existiert eine Pruefungsperiode
+    Angenommen es existiert die geplante Pruefung "Echtzeitsysteme"
+    Wenn ich einen Block mit der geplanten Pruefung "Echtzeitsysteme" erstelle
+    Dann erhalte ich ein Fehlermeldung Pruefungen sind geplant
