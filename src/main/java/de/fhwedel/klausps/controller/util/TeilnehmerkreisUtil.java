5c27c1be1eb944f267344f6fb9b78888e8831387
vanjobanjo <44374624+vanjobanjo@users.noreply.github.com>
2022-03-06 10:47:51 +0100

    inline documentation

--- src/main/java/de/fhwedel/klausps/controller/util/TeilnehmerkreisUtil.java
+++ src/main/java/de/fhwedel/klausps/controller/util/TeilnehmerkreisUtil.java
@@ -3,20 +3,26 @@ package de.fhwedel.klausps.controller.util;
 import de.fhwedel.klausps.model.api.Teilnehmerkreis;
 import java.util.Map;
 
-
+/**
+ * This class is used for utility functionality concerning Teilnehmerkreise.
+ */
 public class TeilnehmerkreisUtil {
 
   /**
-   * No instancing
+   * A utility class providing only static methods, should not be instantiated
    */
   private TeilnehmerkreisUtil() {
 
   }
 
   /**
-   * Will merge to Teilnehmerkreis Schätzung together with following merge bi function: (X,Y) -> X.Schaetzung > Y.Schaetzung
-   * @param collectedTeilnehmerkreise the collected
-   * @param toAdd the to be added
+   * Different Pruefungen can have the same Teilnehmerkreis but with different schaetzungen. When
+   * collecting Teilnehmerkreisschaetzungen this needs to be considered and consolidated. This
+   * method adds all Teilnehmerkreise from a Map and puts for duplicates the bigger schaetzung in
+   * the result map <code>toAdd</code>.
+   *
+   * @param collectedTeilnehmerkreise the collected Teilnehmerkreisschaetzungen with duplicates
+   * @param toAdd                     the consolidated Teilnehmerkreisschaetzungen
    */
   public static void compareAndPutBiggerSchaetzung(
       Map<Teilnehmerkreis, Integer> collectedTeilnehmerkreise,
@@ -24,8 +30,10 @@ public class TeilnehmerkreisUtil {
     for (Map.Entry<Teilnehmerkreis, Integer> schaetzung : toAdd.entrySet()) {
       Integer foundSchaetzung = collectedTeilnehmerkreise.getOrDefault(schaetzung.getKey(), null);
       Integer newSchaetzung = schaetzung.getValue();
+      // look if Teilnehmerkreis is already in toAdd
       if (foundSchaetzung == null) {
         collectedTeilnehmerkreise.put(schaetzung.getKey(), newSchaetzung);
+        // if already in toAdd, exchange if new Teilnehmerkreis has a bigger schaetzung
       } else if (foundSchaetzung < newSchaetzung) {
         collectedTeilnehmerkreise.replace(schaetzung.getKey(), foundSchaetzung, newSchaetzung);
       }
